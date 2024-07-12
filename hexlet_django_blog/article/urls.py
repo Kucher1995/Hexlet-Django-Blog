2f0d8205cd3e100c4bbc75e7530dad876fe6b957
Kucher1995 <sashadovgun@gmail.com>
2024-07-12 18:25:33 +0300

    link to article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, \
+    ArticleView, ArticleCommentsView
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<str:tags>/<int:id>/', views.index, name='articles')
+    path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view(), name='article'),
+    path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,18 +1,30 @@
-
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.shortcuts import render, redirect
+
+from hexlet_django_blog.article.models import Article
 
 
 class IndexView(View):
 
-    def get(request, *args):
-        response = redirect('python/42/')
-        return response
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
 
 
-def index(request, tags, id):
-    return render(
-        request,
-        'articles/index.html',
-        context={'tags': tags, 'id': id},
-    )
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
+
+
+class ArticleCommentsView(View):
+
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, id=kwargs['id'], article__id=kwargs['article_id'])
+
+        return render( ... )
